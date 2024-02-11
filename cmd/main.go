8b19fa7ad785fa3e8a66bec31f66caa16e9fae29
Szilárd Kálosi <kalosi.szilard@gmail.com>
2024-02-11 19:42:09 +0100

    feat: aaa

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, 1231 a111sdas2 play 12312 ß asdasd groun11311d 123")
+	fmt.Println("Hello, 1111231 a111sdas2 play 12312 ß asdasd groun11311d 123")
 }
